eee635e2802c6aa51a6bfa7f93a62a997eb05152
dikivenom <dikivenom@gmail.com>
2024-02-05 18:57:02 +0700

    feat: add new section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       'Bebas-Neue': ['Bebas Neue', 'sans-serif',],
+      'Poppins' : ['poppins', 'sans-serif', 'Open Sans'],
     },
     plugins: [require("daisyui")],
     extend: {},
